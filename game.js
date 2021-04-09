b0144439ebe1d90915fde2306e2a8f4c9b3969c1
kasia-nie <80925420+kasia-nie@users.noreply.github.com>
2021-04-09 21:53:22 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -70,26 +70,26 @@ document.addEventListener('DOMContentLoaded', function() {
 		let field = e.target.closest("td").innerText
 		
 		if (winner == true){ 
-		//if (document.querySelector("#statement").innerText.includes("Wygrana!")){ // jesli jest wygrana to wyjdz i nic nie rob wiecej
+		
 			return;
 		}
 		
-		if (helper === "X" && field == ""){   				// Jesli helper rowna sie X to wykonaj:
-		e.target.closest("td").innerText = "X";				// wpisz w najbliszy td (czyli klikniety td) X
+		if (helper === "X" && field == ""){   				
+		e.target.closest("td").innerText = "X";				
 			wynik("X"); 
 			wynik("O");
 			if (player == "") { 
 				helper = "O";
 			}
-			if (player == "human_play") { 					// i jeśli player ustawiony jest na human play to
-				helper = "O";								// ustaw helpera na 0
+			if (player == "human_play") { 					
+				helper = "O";								
 			}
-			if (player == "computer_play") {				// a jesli player ustawiony jest na computer play	
-				computer_move();							// to wykonaj funkcje computer_move
+			if (player == "computer_play") {					
+				computer_move();							
 			}
 				wynik("X"); 
 				wynik("O");
-		return;												// wyjdz i nie sprawdzaj kolejnego warunku
+		return;												
 		} 
 			
 		if (helper === "O" && field == ""){ 
@@ -111,10 +111,10 @@ document.addEventListener('DOMContentLoaded', function() {
 		} 
 	}) 	
 	
-	// Funkcja ruch komputera      // Znajduje wszystkie puste pola sposrod wszystkich td, dodaje je do tablicy i losuje z nich losowe pole, w ktore wstawia 0 lub X
+	// Funkcja ruch komputera      
 	function computer_move() {
 		if (winner == true) { console.log("sprawdzamy"); return; }
-		//if (document.querySelector("#statement").innerText.includes("Wygrana!")) { console.log("sprawdzamy"); return; }		// jesli jest wygrana to wyjdz i nie wykonuj kodu
+		
 		if (document.querySelector("#statement").innerText.includes("Remis!")) { return;}
 		let new_td = "";
 		let new_td_tab = [];
@@ -124,9 +124,9 @@ document.addEventListener('DOMContentLoaded', function() {
 				new_td_tab.push(new_td);
 			}
 		}
-		//console.log(new_td_tab.length);
+		
 		let random_field = Math.floor((Math.random() * new_td_tab.length) + 1);
-		//console.log(random_field);
+	
 		
 		if (helper === "X") {
 		new_td_tab[random_field-1].innerText = "O";
@@ -155,35 +155,35 @@ document.addEventListener('DOMContentLoaded', function() {
 		let all_td_tab = [];
 		
 		
-		// sprawdzanie wyniku w poziomie (skacze co 3 komorki, bo musi byc sprawdzony 3 razy)
+		
 		for ( i = 0; i < all_td.length; i = i+3 )				
 		{ 
 			if (all_td[i].innerText == C && all_td[i+1].innerText == C && all_td[i+2].innerText == C) { 
 			wygrana(C);
 			}
 		}
-		// sprawdzanie wyniku w pionie (musi byc sprawdzony 3 razy )
+		
 		for ( i = 0; i < 3; i= i+1 )							
 		{ 	
 			if (all_td[i].innerText == C && all_td[i+3].innerText == C && all_td[i+6].innerText == C) { 
 			wygrana(C);
 			}
 		}
-		// sprawdzanie skosow
+		
 			if (all_td[0].innerText == C && all_td[4].innerText == C && all_td[8].innerText == C) { 
 			wygrana(C);
 			}
 			if (all_td[2].innerText == C && all_td[4].innerText == C && all_td[6].innerText == C) { 
 			wygrana(C);
 			}
-		// remis
+		
 		for ( i = 0; i < all_td.length; i = i+1 ) {
 			if (all_td[i].innerText != "" ) {
 				all_td_tab.push(all_td[i]);
 			}}
-		//console.log(all_td_tab.length);
+		
 			if (all_td_tab.length == 9 && winner != true) {
-			//if (all_td_tab.length == 9 && document.querySelector("#statement").innerText !== "Wygrana!") {
+			
 				tie();
 			}
 	}	
